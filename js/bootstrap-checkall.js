0644a502c3ed73e65f682853eef25a1d833c88c4
Vivien Tedesco <vivien.tedesco@gmail.com>
2016-08-17 12:28:03 +0200

    Update bootstrap-checkall.js

--- js/bootstrap-checkall.js
+++ js/bootstrap-checkall.js
@@ -10,3 +10,14 @@ $('[data-toggle=autoCheck-off]').click(function(event) {
         $(this).prop('checked', false); 
     });
 });
+// Check or uncheck controlled by a default checkbox
+$('[data-toggle=autoCheck-toggle]').click(function(event) {
+    if ($(this).is(':checked')){
+        var direction = true;
+    } else{
+        var direction = false;
+    }
+    $( $(this).data('target') ).find('input:checkbox').each(function() {
+        $(this).prop('checked', direction); 
+    });
+});
